592cbf6332ec8abb9329023441836094ba63da33
fymap <fymap@163.com>
2018-11-21 16:13:22 +0800

    m

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 function build(fname) {
-  var txt = fs.readFileSync(path.join(__dirname, 'data/1.txt'), 'utf8');
+  var txt = fs.readFileSync(path.join(__dirname, `data/${fname}.txt`), 'utf8');
   var o = {
     "uuid": "64350b50-a810-4901-b86b-7a5106bdef2c",
     "title": `频道-${fname}`,
