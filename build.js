28a35516ee40ae191028528a6270642244f7b5f6
fymap <fymap@163.com>
2018-11-21 16:10:14 +0800

    modify channel

---
+++ build.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+function build(fname) {
+  var txt = fs.readFileSync(path.join(__dirname, 'data/1.txt'), 'utf8');
+  var o = {
+    "uuid": "64350b50-a810-4901-b86b-7a5106bdef2c",
+    "title": `频道-${fname}`,
+    "channels": []
+  }
+  var channels = [];
+  var lines = txt.split('\r\n');
+  lines.forEach((line) => {
+    if (line) {
+      var chi = line.split(',')
+      channels.push({
+        name: chi[0],
+        url: chi[1]
+      });
+    }
+  });
+  o.channels = channels;
+  fs.writeFileSync(path.join(__dirname, `${fname}.json`), JSON.stringify(o));  
+}
+
+build('huya');
