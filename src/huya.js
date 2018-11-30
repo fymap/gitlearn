52184f6c05478bee3fc4b8483b624c50e362cb7b
fymap <fymap@163.com>
2018-11-30 16:54:17 +0800

    优化

--- src/huya.js
+++ src/huya.js
@@ -1,7 +1,8 @@
 const request = require('request')
+const cheerio = require('cheerio')
 const fs = require('fs')
 const path = require('path')
-function getGames(page=1, gameArr = []) {
+function getGames(page = 1, gameArr = []) {
   let pageSize = 120
   let opts = {
     // url: 'http://www.huya.com/cache.php?m=Game&do=ajaxGetGameLive&gameId=2135&page=1&pageSize=500',
@@ -16,7 +17,7 @@ function getGames(page=1, gameArr = []) {
       'authority': 'm.huya.com',
       'x-requested-with': 'XMLHttpRequest'
     }
-  }  
+  }
   request(opts, (err, response, body) => {
     if (err) {
       console.log(err)
@@ -31,35 +32,160 @@ function getGames(page=1, gameArr = []) {
       if (page < pageNum) {
         getGames(page + 1, gameArr)
       } else {
-        console.log('gameArr.length: ' + gameArr.length)        
-        fs.writeFileSync(path.join(__dirname, '../data/huya.json'), JSON.stringify(gameArr, null, '\t'))
+        console.log('gameArr.length: ' + gameArr.length)
+        fs.writeFileSync(path.join(__dirname, '../huya/huya.json'), JSON.stringify(gameArr, null, '\t'))
         console.log('数据保存成功')
-        geneList(gameArr)
-      }      
+        // geneList(gameArr)
+      }
     }
-  })  
+  })
 }
 
-function getGameByCache () {
-  let data = fs.readFileSync(path.join(__dirname, '../data/huya.json'), 'utf8')
+function getGameByCache() {
+  let data = fs.readFileSync(path.join(__dirname, '../huya/huya.json'), 'utf8')
   geneList(JSON.parse(data))
 }
 
-function geneList(games) {
-  let m3u8Arr = {
+function geneList(games, savedArr = []) {
+  let arr = games.slice(0, 20)
+  const promises = arr.map(el => {
+    return new Promise((resolve, reject) => {
+      request({
+        url: 'https://m.huya.com/' + el.profileRoom,
+        timeout: 20000,
+        headers: {
+          'authority': 'm.huya.com',
+          'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+          'cookie': 'guid=7160c3aafeb4005ca94114ed15ed14cd; Hm_lvt_3a022a5f11ac1cfb68c9bbffeb894709=1543496529,1543550526,1543550722,1543551465; Hm_lpvt_3a022a5f11ac1cfb68c9bbffeb894709=1543551465'
+        }
+      }, (err, response, body) => {
+        if (err) {
+          console.log(err)
+        }
+        if (!err && response.statusCode === 200) {
+          let $ = cheerio.load(body)
+          let link = $('video').attr('src')
+          if (link) {
+            resolve({
+              name: el.introduction,
+              profileRoom: el.profileRoom,
+              url: 'https:' + link
+            })
+          } else {
+            resolve({
+              name: el.introduction,
+              profileRoom: el.profileRoom,
+              url: 'not found'
+            })
+          }
+        } else {
+          resolve({
+            name: el.introduction,
+            profileRoom: el.profileRoom,
+            url: ''
+          })
+        }
+      })
+    })
+  })
+  Promise.all(promises).then(links => {
+    savedArr = savedArr.concat(links)
+    let arr = games.slice(20)
+    console.log('arr.len: ' + arr.length)
+    if (arr.length > 0) {
+      geneList(arr, savedArr)
+    } else {
+      fs.writeFileSync(path.join(__dirname, '../huya/huya_middle.json'), JSON.stringify(savedArr, null, '\t'))
+    }
+  })
+}
+
+function reCheck() {
+  var games = fs.readFileSync(path.join(__dirname, '../huya/huya_middle.json'), 'utf8')
+  games = JSON.parse(games)
+
+  let promises = games.map(game => {
+    return new Promise((resolve, reject) => {
+      if (game.url === '') {
+        request({
+          url: 'https://m.huya.com/' + game.profileRoom,
+          timeout: 20000,
+          headers: {
+            'authority': 'm.huya.com',
+            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+            'cookie': 'guid=7160c3aafeb4005ca94114ed15ed14cd; Hm_lvt_3a022a5f11ac1cfb68c9bbffeb894709=1543496529,1543550526,1543550722,1543551465; Hm_lpvt_3a022a5f11ac1cfb68c9bbffeb894709=1543551465'
+          }
+        }, (err, response, body) => {
+          if (err) {
+            console.log(err)
+          }
+          if (!err && response.statusCode === 200) {
+            let $ = cheerio.load(body)
+            let link = $('video').attr('src')
+            if (link) {
+              game.url = 'https:' + link
+              resolve(game)
+            } else {
+              reject('no link')
+            }            
+          } else {
+            reject(err)
+          }
+        })
+      } else {
+        resolve(game)
+      }
+    })
+  })
+  Promise.all(promises).then(function (games) {
+    console.log(games.length)
+    fs.writeFileSync(path.join(__dirname, '../huya/huya_final.json'), JSON.stringify(games, null, '\t'))
+  }).catch(reason => {
+    console.log(reason)
+  })
+}
+
+function buildM3U8 () {
+  var games = fs.readFileSync(path.join(__dirname, '../huya/huya_final.json'), 'utf8')
+  games = JSON.parse(games)
+  var o = {
     "uuid": "64350b50-a810-4901-b86b-7a5106bdef2c",
     "title": "huya_all",
     "channels": []
   }
-  games.forEach((game) => {
-    m3u8Arr.channels.push({
-      name: game.introduction,
-      url: 'https://aldirect.hls.huya.com' + game.screenshot.substring(game.screenshot.indexOf('com') + 3, game.screenshot.lastIndexOf('/')) + '_1200.m3u8'
-    })    
-  })
-  fs.writeFileSync(path.join(__dirname, '../huya_all.json'), JSON.stringify(m3u8Arr, null, '\t'))  
-  console.log('m3u8文件生成完毕')
+  games.forEach(game => {
+    if (game.url !== '' && game.url !== 'not found') {
+      o.channels.push({
+        name: game.name,
+        url: game.url
+      })
+    }
+  });
+  fs.writeFileSync(path.join(__dirname, '../huya_all.json'), JSON.stringify(o, null, '\t'))
 }
 
-getGames()
-// getGameByCache()
+function main() {
+  console.log(process.argv)
+  let command = process.argv.length > 2 ? process.argv[2] : 'fetch'
+  switch (command) {
+    case 'fetch':
+      getGames()
+      break
+    case 'build':
+      getGameByCache()
+      break
+    case 'check':
+      reCheck()
+      break
+    case 'm3u8':
+      buildM3U8()
+      break
+    default:
+      break;
+  }
+}
+
+main()
+
+
+
