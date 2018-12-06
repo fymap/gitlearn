5a5db7ef8ffdaed4496fa66115a562d235099b62
fymap <fymap@163.com>
2018-12-06 15:47:55 +0800

    add cctv channel

--- src/channel.js
+++ src/channel.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 function build(fname) {
-  var txt = fs.readFileSync(path.join(__dirname, `data/${fname}.txt`), 'utf8');
+  var txt = fs.readFileSync(path.join(__dirname, `../data/${fname}.txt`), 'utf8');
   var o = {
     "uuid": "64350b50-a810-4901-b86b-7a5106bdef2c",
     "title": `频道-${fname}`,
@@ -20,8 +20,9 @@ function build(fname) {
     }
   });
   o.channels = channels;
-  fs.writeFileSync(path.join(__dirname, `${fname}.json`), JSON.stringify(o, null, '\t'));  
+  fs.writeFileSync(path.join(__dirname, `../${fname}.json`), JSON.stringify(o, null, '\t'));  
 }
 
-build('huya');
-build('others');
+// build('huya');
+// build('others');
+build('cctv')
